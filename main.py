c3934c443f3df73283bfa10302d69e9fb8f08492
Patrick Esser <Patrick.Esser@gmx.net>
2017-05-04 14:47:53 +0200

    autoencoder

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config = config)
 import keras.backend as K
 K.set_session(session)
+import keras
 
-import os, logging, shutil, datetime, socket, time
+import os, logging, shutil, datetime, socket, time, math
+import numpy as np
 from multiprocessing.pool import ThreadPool
 from keras.preprocessing.image import ImageDataGenerator
+from PIL import Image
+from tqdm import tqdm, trange
 
 
 data_dir = os.path.join(os.getcwd(), "data")
@@ -76,6 +80,140 @@ def get_batches(img_shape, batch_size):
     return BufferedWrapper(batches)
 
 
+def tile(X, rows, cols):
+    """Tile images for display."""
+    tiling = np.zeros((rows * X.shape[1], cols * X.shape[2], X.shape[3]), dtype = X.dtype)
+    for i in range(rows):
+        for j in range(cols):
+            idx = i * cols + j
+            if idx < X.shape[0]:
+                img = X[idx,...]
+                tiling[
+                        i*X.shape[1]:(i+1)*X.shape[1],
+                        j*X.shape[2]:(j+1)*X.shape[2],
+                        :] = img
+    return tiling
+
+
+def plot_images(X, name):
+    X = (X + 1.0) / 2.0
+    X = np.clip(X, 0.0, 1.0)
+    rc = math.sqrt(X.shape[0])
+    rows = cols = math.ceil(rc)
+    canvas = tile(X, rows, cols)
+    fname = os.path.join(out_dir, name + ".png")
+    Image.fromarray(np.uint8(255*canvas)).save(fname)
+
+
+def keras_conv(x, kernel_size, filters, stride = 1):
+    x = keras.layers.Conv2D(
+            filters = filters,
+            kernel_size = kernel_size,
+            strides = stride,
+            padding = "SAME",
+            kernel_initializer = "he_normal")(x)
+    return x
+
+
+def keras_upsampling(x):
+    x = keras.layers.UpSampling2D()(x)
+    return x
+
+
+def keras_activate(x):
+    x = keras.layers.Activation("relu")(x)
+    return x
+
+
+class Model(object):
+    def __init__(self, img_shape, n_total_steps):
+        self.img_shape = img_shape
+        self.initial_learning_rate = 1e-3
+        self.end_learning_rate = 0.0
+        self.n_total_steps = n_total_steps
+        self.log_frequency = 250
+        self.define_graph()
+        self.init_graph()
+
+
+    def make_ae(self):
+        n_features = 64
+
+        x = keras.layers.Input(shape = self.img_shape)
+        features = x
+        for i in range(4):
+            features = keras_conv(features, 3, (i + 1)*n_features, stride = 2)
+            features = keras_activate(features)
+        features = keras_conv(features, 1, K.int_shape(features)[-1])
+        features = keras_activate(features)
+        for i in reversed(range(4)):
+            features = keras_upsampling(features)
+            features = keras_conv(features, 3, (i + 1)*n_features)
+            features = keras_activate(features)
+        output = keras_conv(features, 3, 3)
+
+        return keras.models.Model(x, output)
+
+
+    def define_graph(self):
+        self.train_ops = {}
+        self.log_ops = {}
+        self.img_ops = {}
+
+        global_step = tf.Variable(0, trainable = False)
+        ae = self.make_ae()
+
+        x = tf.placeholder(tf.float32, shape = (None,) + self.img_shape)
+        x_rec = ae(x)
+        loss = tf.reduce_mean(tf.abs(x - x_rec))
+
+        learning_rate = tf.train.polynomial_decay(
+                learning_rate = self.initial_learning_rate,
+                global_step = global_step,
+                decay_steps = self.n_total_steps,
+                end_learning_rate = self.end_learning_rate,
+                power = 1.0)
+        optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+        train = optimizer.minimize(loss, global_step = global_step)
+
+        self.inputs = {"x": x}
+        self.train_ops = {"train": train}
+        self.log_ops = {
+                "loss": loss,
+                "global_step": global_step,
+                "learning_rate": learning_rate}
+        self.img_ops = {"x": x, "g": x_rec}
+
+
+    def init_graph(self):
+        session.run(tf.global_variables_initializer())
+
+
+    def fit(self, batches):
+        for batch in trange(self.n_total_steps):
+            X_batch = next(batches)
+            feed_dict = {
+                    self.inputs["x"]: X_batch}
+            fetch_dict = {"train": self.train_ops}
+            if self.log_ops["global_step"].eval(session) % self.log_frequency == 0:
+                fetch_dict["log"] = self.log_ops
+                fetch_dict["img"] = self.img_ops
+            result = session.run(fetch_dict, feed_dict)
+            self.log_result(result)
+
+
+    def log_result(self, result):
+        if "log" in result:
+            for k, v in result["log"].items():
+                if type(v) == float:
+                    v = "{:.4e}".format(v)
+                logging.info("{}: {}".format(k, v))
+        if "img" in result:
+            global_step = self.log_ops["global_step"].eval(session)
+            for k, v in result["img"].items():
+                plot_images(v, k + "_{:07}".format(global_step))
+
+
 if __name__ == "__main__":
     img_shape = (128, 128, 3)
     batch_size = 64
@@ -83,3 +221,8 @@ if __name__ == "__main__":
     init_logging()
     batches = get_batches(img_shape, batch_size)
     logging.info("Number of samples: {}".format(batches.n))
+
+    n_epochs = 100
+    n_total_steps = int(n_epochs * batches.n / batch_size)
+    model = Model(img_shape, n_total_steps)
+    model.fit(batches)
